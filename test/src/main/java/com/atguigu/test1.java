ee1f0543ea3dcf50178d453858dbb8d1b8b00acc
yinling123 <2048009927@qq.com>
2022-08-06 20:53:55 +0800

    yes1

--- test/src/main/java/com/atguigu/test1.java
+++ test/src/main/java/com/atguigu/test1.java
@@ -1,4 +1,9 @@
 package com.atguigu;
 
 public class test1 {
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(22222);
+        System.out.println(33333);
+    }
 }
